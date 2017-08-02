58afa0f4a5e187374c85a2df3ed8be05a2ad4d51
Arthurburkey <Arthurburkey@email.com>
2017-08-02 11:21:59 -0700

    uploaded Project Files for Postal Calculator

---
+++ PostalCalculatorHelperMethods/WebForm1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="PostalCalculatorHelperMethods.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Postal Calculator<br />
+        <br />
+        Width:<asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="widthTextBox_TextChanged"></asp:TextBox>
+        <br />
+        Height:<asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="heightTextBox_TextChanged"></asp:TextBox>
+        <br />
+        Length:<asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="lengthTextBox_TextChanged"></asp:TextBox>
+        <br />
+        <br />
+        <asp:RadioButton ID="groundButton" runat="server" Text="Ground" AutoPostBack="True" GroupName="methodGroup" OnCheckedChanged="groundButton_CheckedChanged" />
+        <br />
+        <asp:RadioButton ID="airButton" runat="server" Text="Air" AutoPostBack="True" GroupName="methodGroup" OnCheckedChanged="airButton_CheckedChanged" />
+        <br />
+        <asp:RadioButton ID="nextDayButton" runat="server" Text="Next Day" AutoPostBack="True" GroupName="methodGroup" OnCheckedChanged="nextDayButton_CheckedChanged" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ PostalCalculatorHelperMethods/WebForm1.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PostalCalculatorHelperMethods
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        private void performChanged()
+        {
+            //throw new NotImplementedException();
+            if (!valuesExist()) return;
+            int volume = 0;
+            if (!tryGetVolume(out volume)) return;
+            double postageMultiplier = getPostageMultiplier();
+            double cost = volume * postageMultiplier;
+            resultLabel.Text = String.Format("Your parcel will cost {0} to ship.", cost);
+
+        }
+
+        private double getPostageMultiplier()
+        {
+            if (groundButton.Checked) return .15;
+            else if (airButton.Checked) return .25;
+            else if (nextDayButton.Checked) return .45;
+            else return 0; 
+        }
+
+        private bool tryGetVolume(out int volume)
+        {
+            volume = 0;
+            int width = 0;
+            int height = 0;
+            int length = 0;
+
+            if (!int.TryParse(widthTextBox.Text.Trim(), out width)) return false;
+            if (!int.TryParse(heightTextBox.Text.Trim(), out height)) return false;
+            if (!int.TryParse(lengthTextBox.Text.Trim(), out length)) length = 1;
+
+            volume = width * height * length;
+            return true;
+        }
+
+        private bool valuesExist()
+        {
+            if (!airButton.Checked
+                && !groundButton.Checked
+                && !nextDayButton.Checked)
+                return false;
+
+            if (widthTextBox.Text.Trim().Length == 0
+                || heightTextBox.Text.Trim().Length == 0)
+                return false;
+            return true; 
+
+        }
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void groundButton_CheckedChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void airButton_CheckedChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void nextDayButton_CheckedChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+    }
+}
